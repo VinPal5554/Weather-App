47ab91b51439bca3243dbd472ed3567cff6bf5a3
Vin <vinaypal5555@gmail.com>
2025-02-25 00:46:39 +0000

    Json formatting and control flow
    Further formatted returned json so that it prints temperature, description, humidity, and wind speed only. User now has choice to keep choosing cities or exit the program.

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -1,19 +1,38 @@
 import requests
 
-# Replace 'your_api_key' with your actual API key
+# Replace with actual API key
 API_KEY = ""
-CITY = input("Location: ")
-URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+program_running = True
 
-# Make a request to the weather API
-response = requests.get(URL)
+while program_running:
+    CITY = input("Location: ")
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
-# Extract and convert successful request to json, otherwise print error
-if response.status_code == 200:
-    weather_data = response.json()
-    print(weather_data)
-else:
-    print(f"Something went wrong extracting data (Status Code: {response.status_code})")
+    # Make a request to the weather API
+    response = requests.get(URL)
+
+    # Extract and convert successful request to json, otherwise print error
+    if response.status_code == 200:
+        weather_data = response.json()
+
+        # Format json appropriately
+        temperature = weather_data["main"]["temp"]
+        weather_description = weather_data["weather"][0]["description"]
+        humidity = weather_data["main"]["humidity"]
+        wind_speed = weather_data["wind"]["speed"]
+
+        print(f"The temperature in {CITY} is: {temperature} degrees")
+        print(f"Description: {weather_description}")
+        print(f"Humidity: {humidity}")
+        print(f"Wind speed: {wind_speed}")
+
+        user_choice = input("Would you like to check again? (Y/N)").lower()
+
+        if user_choice == "n":
+            break
+
+    else:
+        print(f"Something went wrong extracting data (Status Code: {response.status_code})")
 
 
 
