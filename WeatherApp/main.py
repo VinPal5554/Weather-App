6e08cbdb2cc874a7d613aca30d693cea1dc35bb0
Vin <vinaypal5555@gmail.com>
2025-02-25 00:23:55 +0000

    Basic error handling
    Made it so that the user is able to input the corresponding city. Adjusted API logic so that if the request was OK then convert and print out information, otherwise print an error with the corresponding status code.

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -2,14 +2,18 @@ import requests
 
 # Replace 'your_api_key' with your actual API key
 API_KEY = ""
-CITY = "London"
+CITY = input("Location: ")
 URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 # Make a request to the weather API
 response = requests.get(URL)
 
-# Convert response to JSON
-weather_data = response.json()
+# Extract and convert successful request to json, otherwise print error
+if response.status_code == 200:
+    weather_data = response.json()
+    print(weather_data)
+else:
+    print(f"Something went wrong extracting data (Status Code: {response.status_code})")
 
-# Print the weather details
-print(weather_data)
+
+
